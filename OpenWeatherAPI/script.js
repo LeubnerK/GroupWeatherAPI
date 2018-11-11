a07aa869b3dc6268a469e8a00ec671498dc5e60b
Unknown <leubnerk@otc.edu>
2018-11-11 13:06:29 -0600

    Changed php key, added some script, gave p an id. Object is shown in Console for ease of access.

--- OpenWeatherAPI/script.js
+++ OpenWeatherAPI/script.js
@@ -4,3 +4,59 @@
 // Team Members: Kimberly Leubner, Daniel Diaz, Curtis Kesner
 // Term: Fall 2018
 
+var weatherReport;
+var httpRequest = false;
+
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    }
+    catch (requestError) {
+        document.querySelector("p.error").innerHTML = "Forecast not supported by your browser.";
+        document.querySelector("p.error").style.display = "block";
+        return false;
+    }
+    return httpRequest;
+}
+
+function getWeather(evt) {
+
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+        httpRequest.onreadystatechange = displayWeather;
+    }
+ 
+    httpRequest.abort();
+    httpRequest.open("get", "weather.php?", true);
+    httpRequest.send(null);
+ }
+ 
+ function displayWeather()
+ {
+     if (httpRequest.readyState == 4 && httpRequest.status == 200)
+     {
+         // Stage 4
+         var weatherReport = httpRequest.responseText;
+         var jsObject = JSON.parse(weatherReport);
+         console.log(jsObject);
+
+        // Temperature, covnerted from Kelvin to F
+        var temp = Math.round((jsObject.main.temp - 273.15)*9/5 + 32);
+
+        // Humidity
+        var humid = jsObject.main.humidity;
+
+        // Forecast string (will add more info)
+        var forecast = "Temperature: " + temp + " °F<br/> Humidity: " + humid + "%";
+
+        // Replaces forecast paragraph html with forecast string
+         document.getElementById("forecast").innerHTML = forecast;
+     }
+ }
+ 
+ 
+ if (window.addEventListener) {
+    window.addEventListener("load", getWeather, false);
+ } else if (window.attachEvent) {
+    window.attachEvent("onload", getWeather);
+ }
--- OpenWeatherAPI/weather.php
+++ OpenWeatherAPI/weather.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "https://api.forecast.io/forecast/c31c5d41033f98d090a49e455dadf686/" . $_GET["lat"] . "," . $_GET["lng"];
+$WeatherSource = "http://api.openweathermap.org/data/2.5/weather?lat=39.0845&lon=-94.5631&APPID=3a2aa7ae6f8e7a9577211ea5cec2a2be";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
